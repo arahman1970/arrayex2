816888b4e723c32c38c160d75fe1bd96b9c6ec79
arahman1976 <ar.nj.usa@gmail.com>
2019-06-19 17:04:00 -0400

    in porcess

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Write a program to print a string in reverse:
+        System.out.println("#1" +"\n");
+        String [] abcd =["Welcome"];
+
+        for (int n =9; n>-1; n--){
+            System.out.println(" In reverse " +num10[n]);
+        }
+
+
+    }
+}
